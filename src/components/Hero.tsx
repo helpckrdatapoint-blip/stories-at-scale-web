ff52313fcfdfaf4e58194fd6ca3b1e826e057788
helpckrdatapoint-blip <help.ckrdatapoint@gmail.com>
2025-11-16 17:26:44 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from "lucide-react";
 import CircularGallery from "@/components/ui/CircularGallery";
 
 const Hero = () => {
-  const words = ["scale," "brand," "boost,"];
+  const words = ["scale," "boost,"];
   
   const scrollToSection = (sectionId: string) => {
     const element = document.getElementById(sectionId);
